6ca636a27f1ff8c6e21f63545eb2a49c3d72ea5e
GeorgianaNicolae <georgiana.nicolae@madetech.com>
2022-07-05 18:56:32 +0300

    React Weather app + Udemy React and TDD

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import SignUpPage from './pages/SignUpPage';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import UserPage from './pages/UserPage';
+import TodayWeatherPage from './pages/TodayWeatherPage';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div data-testid="navBar">
+        <a href='/' title='Home'>Hoaxify</a>
+        <a href='/signup' title='Sign Up'>Sign Up</a>
+      </div>
+      {window.location.pathname === "/" && <HomePage />}
+      {window.location.pathname === "/signup" && <SignUpPage />}
+      {window.location.pathname === "/login" && <LoginPage />}
+      {window.location.pathname.startsWith('/user/') && <UserPage />}
+      {window.location.pathname === "/todayweather" && <TodayWeatherPage />}
+
+
     </div>
   );
 }
